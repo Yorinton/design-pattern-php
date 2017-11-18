3054c85d732a86e04cbf2bcc405b62a80d20121c
Yorinton <ka2ki.yori@outlook.com>
2017-11-18 16:21:21 +0000

    array_mapとforeach参照渡し比較テスト追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use DesignPattern\Repository\Pattern3\TodoItemRepository;
+use DesignPattern\Repository\TodoItemRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            TodoItemRepositoryInterface::class,
+            TodoItemRepository::class
+        );
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -12,8 +12,41 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testForeachReference()
     {
+        $arr = array(1,2,3,4);
+        //$arrの各要素への参照に各要素の2倍を入れる形になる
+        //元の$arrの値が変わる
+        foreach($arr as &$value){
+            $value = $value * 2;
+        }
+        unset($value);
+        var_dump($arr);
+
+        $this->assertTrue(true);
+    }
+
+    public function testForeachNoReference()
+    {
+        $arr2 = array(3,4,5,6);
+        //参照ではないため、元の$arr3の値は変わらない
+        foreach($arr2 as $value){
+            echo "\n".$value * 2 ."\n";
+        }
+        var_dump($arr2);
+        $this->assertTrue(true);
+    }
+
+    public function testArrayMap()
+    {
+        $arr3 = array(2,4,6,8);
+
+        $arr3 = array_map(function($value){
+            //ここでreturnした値が配列の各要素に格納される
+            return $value * 3;
+        },$arr3);
+
+        var_dump($arr3);
         $this->assertTrue(true);
     }
 }
